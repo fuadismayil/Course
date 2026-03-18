59d0bfc4910d301c3ec2faff8c77106efa5228c1
fuadismayil <fuadzi-apa103@code.edu.az>
2026-03-18 12:49:58 +0400

    BaseUser created

---
+++ Course/Models/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course.Models
+{
+    public class BaseUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,18 +1,12 @@
 ﻿using Course.Actions;
 using Course.Models;
 using Microsoft.Win32;
-
 namespace Course
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            //================================================================================================================================================
-            //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$--Student--$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-            //================================================================================================================================================
-
             Student student1 = new()
             {
                 FirstName = "Murad",
@@ -55,7 +49,7 @@ namespace Course
                 BirthDate = new DateOnly(2000, 5, 14),
                 Address = "Baki səhəri, Azerbaycan",
                 Email = "ali.mammadov@example.com",
-                Phone = "+994501234567",
+                PhoneNumber = "+994501234567",
                 Password = "12345"
             };
 
@@ -63,62 +57,6 @@ namespace Course
             Login.LoginTeacher("ali.mammadov@example.com", "123456");
 
 
-
-
-
-            //Student student1 = new Student("Murad", "Mammadov", "murad.mammadov@gmail.com", "Murad123!!", "muradmm-dev", 0501234567, new DateOnly(2004, 5, 14), "APA101", 87, "Bakı şəhəri, Nərimanov rayonu");
-            //Console.WriteLine(student1.GetName());
-            //Console.WriteLine(student1.GetGroup());
-            //Console.WriteLine(student1.GetStudent());
-            //Console.WriteLine(student1.GetEmail());
-            //Console.WriteLine(student1.GetBirthDate());
-            //Console.WriteLine(student1.GetContact());
-
-            //Student student2 = new Student("Tural", "Qasımov", "tural.qasimov@gmail.com", "Tural456!!", "turalq-dev", 701112233);
-            //Console.WriteLine(student2.GetName());
-            //Console.WriteLine(student2.GetGroup());
-            //Console.WriteLine(student2.GetStudent());
-            //Console.WriteLine(student2.GetEmail());
-            //Console.WriteLine(student2.GetBirthDate());
-            //Console.WriteLine(student2.GetContact());
-
-            //Student student3 = new Student("Elvin", "Rzayev", "elvin.rzayev@gmail.com", "Elvin2024!!", "elvinr-dev", new DateOnly(2001, 12, 1), "APA103");
-            //Console.WriteLine(student3.GetName());
-            //Console.WriteLine(student3.GetGroup());
-            //Console.WriteLine(student3.GetStudent());
-            //Console.WriteLine(student3.GetEmail());
-            //Console.WriteLine(student3.GetBirthDate());
-            //Console.WriteLine(student3.GetContact());
-
-
-            //================================================================================================================================================
-            //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$--Teacher--$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-            //================================================================================================================================================
-
-
-            //Teacher teacher1 = new Teacher("Aysel","Hüseynova","ayselh-dev","Computer Science","Programming",new DateOnly(1987, 3, 12),"Bakı şəhəri, Yasamal rayonu","aysel.huseynova@gmail.com","0501234567",12345);
-            //Console.WriteLine(teacher1.GetName());
-            //Console.WriteLine(teacher1.GetSubject());
-            //Console.WriteLine(teacher1.GetTeacher());
-            //Console.WriteLine(teacher1.GetEmail());
-            //Console.WriteLine(teacher1.GetBirthDate());
-            //Console.WriteLine(teacher1.GetContact());
-
-            //Teacher teacher2 = new Teacher("Reşad","Memmedov","rashadm-dev","rashad.mammadov@gmail.com", "0709876543", 54321);
-            //Console.WriteLine(teacher2.GetName());
-            //Console.WriteLine(teacher2.GetSubject());
-            //Console.WriteLine(teacher2.GetTeacher());
-            //Console.WriteLine(teacher2.GetEmail());
-            //Console.WriteLine(teacher2.GetBirthDate());
-            //Console.WriteLine(teacher2.GetContact());
-
-            //Teacher teacher3 = new Teacher("Leyla","Quliyeva","leylaq-dev","leyla.quliyeva@gmail.com","Mathematics",new DateOnly(1990, 11, 25)  );
-            //Console.WriteLine(teacher3.GetName());
-            //Console.WriteLine(teacher3.GetSubject());
-            //Console.WriteLine(teacher3.GetTeacher());
-            //Console.WriteLine(teacher3.GetEmail());
-            //Console.WriteLine(teacher3.GetBirthDate());
-            //Console.WriteLine(teacher3.GetContact());
         }
     }
 }
